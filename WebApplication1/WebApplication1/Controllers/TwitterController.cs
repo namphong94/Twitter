422e743ee87da1be630cd1d125a9345eb878cf17
phongpham <phongpham@kms-technology.com>
2015-05-21 17:12:44 +0700

    phong

---
+++ WebApplication1/WebApplication1/Controllers/TwitterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TwitterController : Controller
+    {
+        // GET: Twitter
+        public string Index(string status)
+        {
+            TwitterClient test = new TwitterClient();
+            test.POST(status);
+
+            return "Hello, Twitter!!!";
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/TwitterClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class TwitterClient
+    {
+        public void POST(string tweet)
+        {
+            string status = tweet;
+
+            var oauth_token = "787239091-ayyTqprgWJKj0Ui6EKWMfsSFUJSsBHGmQFJRLcNU";
+            var oauth_token_secret = "fByhJbD9w4irfNBSrHxkXXNW8SeP0Sfe02s2NHa3noKOg";
+            var oauth_consumer_key = "m5cDRZohTMUeTWAXXzamSODMX";
+            var oauth_consumer_secret = "390Nxp3aMulBiRxfS3MAkqBXePwFzg8Wpp2j1jZBWTillKjPzh";
+
+            var oauth_version = "1.0";
+            var oauth_signature_method = "HMAC-SHA1";
+            var oauth_nonce = Convert.ToBase64String(
+                                              new ASCIIEncoding().GetBytes(
+                                                   DateTime.Now.Ticks.ToString()));
+            var timeSpan = DateTime.UtcNow
+                                              - new DateTime(1970, 1, 1, 0, 0, 0, 0,
+                                                   DateTimeKind.Utc);
+            var oauth_timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+            var resource_url = "https://api.twitter.com/1.1/statuses/update.json";
+
+            var baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+                "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&status={6}";
+
+            var baseString = string.Format(baseFormat,
+                                        oauth_consumer_key,
+                                        oauth_nonce,
+                                        oauth_signature_method,
+                                        oauth_timestamp,
+                                        oauth_token,
+                                        oauth_version,
+                                        Uri.EscapeDataString(status)
+                                        );
+
+            baseString = string.Concat("POST&", Uri.EscapeDataString(resource_url),
+                         "&", Uri.EscapeDataString(baseString));
+
+            var compositeKey = string.Concat(Uri.EscapeDataString(oauth_consumer_secret),
+                        "&", Uri.EscapeDataString(oauth_token_secret));
+
+            string oauth_signature;
+            using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(compositeKey)))
+            {
+                oauth_signature = Convert.ToBase64String(
+                    hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
+            }
+
+            var headerFormat = "OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+                   "oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+                   "oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+                   "oauth_version=\"{6}\"";
+
+            var authHeader = string.Format(headerFormat,
+                                    Uri.EscapeDataString(oauth_nonce),
+                                    Uri.EscapeDataString(oauth_signature_method),
+                                    Uri.EscapeDataString(oauth_timestamp),
+                                    Uri.EscapeDataString(oauth_consumer_key),
+                                    Uri.EscapeDataString(oauth_token),
+                                    Uri.EscapeDataString(oauth_signature),
+                                    Uri.EscapeDataString(oauth_version)
+                            );
+
+            var postBody = "status=" + Uri.EscapeDataString(status);
+
+            ServicePointManager.Expect100Continue = false;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resource_url);
+            request.Headers.Add("Authorization", authHeader);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            using (Stream stream = request.GetRequestStream())
+            {
+                byte[] content = ASCIIEncoding.ASCII.GetBytes(postBody);
+                stream.Write(content, 0, content.Length);
+            }
+            WebResponse response = request.GetResponse();
+        }
+    }
+}
