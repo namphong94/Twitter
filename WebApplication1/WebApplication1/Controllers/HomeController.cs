2074759230383977fab96b067ebb12a97af8abb2
phongpham <phongpham@kms-technology.com>
2015-05-22 17:14:21 +0700

    done may 22

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,13 +12,27 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            TwitterClient tweet = new TwitterClient();
+            dynamic jsondata = JsonConvert.DeserializeObject(tweet.GetStatus());
+            var list = new List<TwitterModels>();
+            foreach(var item in jsondata)
+            {
+                var tweetobj = new TwitterModels();
+                tweetobj.Name = item.user.name;
+                tweetobj.Text = item.text;
+                tweetobj.Time = item.created_at;
+                list.Add(tweetobj);
+            }
+            
+            return View(list);
         }
 
         public ActionResult Post (string status)
         {
             TwitterClient test = new TwitterClient();
+            test.GetStatus();
             test.POST(status);
+            
             return RedirectToAction("Index");
         }
 
